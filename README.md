2aa91cdeb6260f780c6c6d765cac958c919b2d38
Darya <dashav1605@mail.ru>
2022-02-12 20:56:05 +0500

    Refactoring

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ElementList
+# Country List
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.1.3.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/element-list'),
+      dir: require('path').join(__dirname, './coverage/country-list'),
       subdir: '.',
       reporters: [
         { type: 'html' },
